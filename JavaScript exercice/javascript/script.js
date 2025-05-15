456b50521695258c4d5517fcc18de65f376fa98e
Damonsas <danh.kh.pro@gmail.com>
2025-05-15 12:38:48 +0200

    update

---
+++ JavaScript exercice/javascript/script.js
@@ -0,0 +1,35 @@
+
+    const premon = "Danh";
+    let age = 25 ;
+    age = age + 1;
+
+    console.log("Je m'appelle" + premon + "j'ai" + age + "ans l'année prochaine");
+
+
+
+    const longueur = 10;
+    const largeur = 5;
+
+    surface = longueur * largeur;
+
+    console.log("Le rectangle de longueur " + longueur +"cm" + " et de " + largeur + "à pour surface " + surface + "cm²");
+
+
+
+    let temperatuceCelsius = 0;
+    let temperatureKelvin = 273.15;
+
+    console.log("La température de " + temperatuceCelsius + "°C est égale à " + (temperatuceCelsius + temperatureKelvin) + "K");
+
+
+    let nombre = (Math.random() * 100).toFixed(0);
+    console.log("Le nombre aléatoire est " + nombre);
+
+
+
+    let rayon = 10;
+    let pi = Math.PI;
+
+    console.log("Le cercle de rayon " + rayon + "cm" + " à pour surface " + (pi * Math.pow(rayon, 2)) + "cm²");
+
+
---
+++ JavaScript exercice2/javascript/script.js
@@ -0,0 +1,17 @@
+let premon = prompt("Danh");
+let age = prompt(25)
+age = age + 1;
+
+console.log("Je m'appelle " + informations + " et j'ai " + age + " ans l'année prochaine");
+
+
+
+const longueur = prompt (10);
+    const largeur = prompt (5);
+
+    surface = longueur * largeur;
+
+    console.log("Le rectangle de longueur " + longueur +"cm" + " et de " + largeur + "à pour surface " + surface + "cm²");
+
+
+
